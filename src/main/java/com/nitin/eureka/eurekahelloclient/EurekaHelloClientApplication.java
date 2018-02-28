f38756d575f4ffa6eb685409f05f9f7efdc0042a
Garg <nitin.garg@centurylink.com>
2018-02-28 10:44:15 -0700

    first commit

---
+++ src/main/java/com/nitin/eureka/eurekahelloclient/EurekaHelloClientApplication.java
@@ -0,0 +1,12 @@
+package com.nitin.eureka.eurekahelloclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaHelloClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaHelloClientApplication.class, args);
+	}
+}
