ed042e807d809b82744c3f9f02d7183b0d5488b8
ab73480 <ab73480@X7016567.ctl.intranet>
2018-02-28 14:14:36 -0700

    updates

---
+++ src/main/java/com/nitin/eureka/eurekahelloclient/resource/HelloResource.java
@@ -0,0 +1,23 @@
+package com.nitin.eureka.eurekahelloclient.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/rest/hello/client")
+public class HelloResource {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping
+	public String hello() {
+		String url = "http://hello-server-eureka-example/rest/hello/server";
+		return restTemplate.getForObject(url, String.class);
+
+	}
+
+}
